e0cc6edaea46335364ec0062ce763834a743cbd0
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-05-09 08:44:00 +0200

    Default parameters for a proper simulation

--- src/phill/solver.py
+++ src/phill/solver.py
@@ -1,3 +1,4 @@
+from snek5000 import logger
 from snek5000.info import InfoSolverMake
 from snek5000.solvers.kth import SimulKTH
 from phill.templates import box, size
@@ -29,6 +30,96 @@ class SimulPhill(SimulKTH):
 
     """
     InfoSolver = InfoSolverPhill
+
+    @staticmethod
+    def _complete_params_with_default(params):
+        """Add missing default parameters."""
+        params = SimulKTH._complete_params_with_default(params)
+        params.nek.velocity._set_attrib("advection", True)
+        return params
+
+    @classmethod
+    def create_default_params(cls):
+        """Set default values of parameters as given in reference
+        implementation.
+
+        """
+        params = super().create_default_params()
+
+        # Set par file parameters
+        # ------------------------
+        # Alternative can be to add phill.par to the current directory and
+        # Synchronize baseline parameters as follows:
+        # -----------------------------------------------------------------
+        #  primary_par_file = get_root() / "phill.par"
+        #  if mpi.rank == 0:
+        #      logger.info(f"Reading baseline parameters from {primary_par_file}")
+        #
+        #  params.nek._read_par(primary_par_file)
+
+        # https://github.com/KTH-Nek5000/KTH_Examples/blob/master/phill_STAT/phill.par
+        general = params.nek.general
+        general.stop_at = "num_steps"
+        general.num_steps = 20
+        general.dt = -2e4
+        general.time_stepper = 'bdf3'
+        general.variable_dt = False
+        general.target_cfl = 0.3
+        general.write_control = 'timestep'
+        general.write_interval = 100
+        general.dealiasing = True
+        general.filtering = 'explicit'
+        general.filter_weight = 0.02
+        general.filter_cutoff_ratio = 0.67
+
+        params.nek.problemtype.stress_formulation = False
+        params.nek.problemtype.variable_properties = False
+
+        params.nek.pressure.residual_tol = params.nek.velocity.residual_tol = 1e-8
+        params.nek.pressure.residual_proj = params.nek.velocity.residual_proj = 1e-8
+        params.nek.velocity.density = 1.
+        params.nek.velocity.viscosity = -700
+
+        # User parameters for KTH framework
+        params.nek.stat.av_step = 10
+        params.nek.stat.io_step = 50
+
+        # Set box file parameters
+        # -----------------------
+        # https://github.com/KTH-Nek5000/KTH_Examples/blob/master/phill_STAT/phill.box
+        oper = params.oper
+        logger.info(oper._doc)
+
+        oper.dim = 3
+        oper.scalars = 1
+        oper.nx = 22
+        oper.ny = 16
+        oper.nz = 19
+
+        oper.Lx = oper.Ly = oper.Lz = 1.
+        oper.boundary = ['P', 'P', 'W', 'W', 'P', 'P']
+
+        # Set SIZE file parameters
+        # ------------------------
+        # https://github.com/KTH-Nek5000/KTH_Examples/blob/master/phill_STAT/SIZE
+        # Basic
+        logger.info(oper.elem._doc)
+        logger.info(oper.max._doc)
+        logger.info(oper.misc._doc)
+
+        oper.elem.order = 6  # lx1
+        oper.elem.coef_dealiasing = 2 / 3  # lxd
+        oper.elem.staggered = True  # Pn Pn-2
+
+        # lelg calculated automatically
+        oper.nproc_min = 8  # lpmin
+        oper.nproc_max = 32  # lpmax
+
+        # BDF3
+        oper.max.order_time = 3
+
+        oper.misc.fast_diag = False
+        return params
 
     def __init__(self, params):
         super().__init__(params)
